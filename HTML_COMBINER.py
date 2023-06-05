4a4f489d7c82a25a0004e5e2e014eea14f7bb061
Saad Gmira <sgmira@e2r10p15.1337.ma>
2023-06-05 20:27:47 +0100

    HTML_Combiner

---
+++ HTML_COMBINER.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+urls = [
+    'https://www.lipsum.com/',
+    'https://www.lipsum.com/',
+    'https://www.lipsum.com/',
+    'https://www.lipsum.com/',
+    'https://www.lipsum.com/',
+]
+
+with open('output.html', 'a', encoding='utf-8') as file:
+    for url in urls:
+        response = requests.get(url)
+        html = response.text
+
+        soup = BeautifulSoup(html, 'html.parser')
+
+        file.write(soup.prettify())
+
+print("HTML saved to file: output.html")
+
